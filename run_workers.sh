ca5a4127ded5b9cb9f31c29d66ff3a1300cbe10b
Christian Sieber <c.sieber@tum.de>
2018-09-06 13:40:17 +0200

    Adepted run_workers.sh

--- run_workers.sh
+++ run_workers.sh
@@ -10,6 +10,12 @@ IMAGE="fginet/docker-video-encoding:latest"
 RESULTS="$HOME/WEBDAV/results/"
 JOBS="$HOME/WEBDAV/jobs/"
 
+SFTP_USER=""
+SFTP_PASSWORD=""
+SFTP_HOST=""
+SFTP_PORT=""
+SFTP_TARGET_DIR=""
+
 # Overwrite here for testing
 CORES=2
 RESULTS="$HOME/docker-video-encoding/samples/results/"
@@ -24,7 +30,11 @@ docker pull $IMAGE
 for (( c=1; c<$CORES; c++ ))
 do
         WID="${HOST}x${c}"
-        WORKER_CMD="python3 worker.py -v \"$VIDEOS\" -t \"$TMP\" -r \"$RESULTS\" -j \"$JOBS\" -c "$IMAGE" -i "$WID" -p $c"
+        WORKER_CMD="python3 worker.py -v \"$VIDEOS\" -t \"$TMP\" -r \"$RESULTS\" -j \"$JOBS\" \
+		    -c "$IMAGE" -i "$WID" -p $c" \
+		    --sftp-user="$SFTP_USER" --sftp-password="$SFTP_PASSWORD" \
+		    --sftp-host="$SFTP_HOST" --sftp-port="$SFTP_PORT" \
+		    --sftp-target-dir="$SFTP_TARGET_DIR"
 
 	echo "Running worker $WID"
         echo screen -dm -S "$WID" bash -c "${WORKER_CMD}; exec bash"
