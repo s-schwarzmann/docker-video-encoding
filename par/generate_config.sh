2dae8eb13bffc97fb29f57ff2f28772bf175bf86
Susanna Schwarzmann <susanna@inet.tu-berlin.de>
2018-09-21 14:31:29 +0200

    bug_Fix

--- par/generate_config.sh
+++ par/generate_config.sh
@@ -4,8 +4,8 @@
 
 
 #list of videos
-declare -a id1="Meridian3840x2160,.mkv"
-declare -a id2="Meridian3840x2160,"
+declare -a id1="BigBuckBunny4000x2250.avi"
+declare -a id2="BigBuckBunny4000x2250"
 
 
 #list all crf values
@@ -26,12 +26,12 @@ do
 	for md in "${max_durs[@]}"
 	do
 			encoder="x264"
-			file_id="$id2"_"$c"-"$md"-"$encoder"-"var"
-			printf "{\"video\": \"$id1\", \n \"crf\": $c, \n \"min_length\": 0, \n \"max_length\": $md, \n \"target_seg_length\": \"var\", \n \"encoder\": \"$encoder\"}"> jobs_2/"$file_id".txt 
+			file_id="$id2"_"$c"-"$md"-"$encoder"-"fix"
+			printf "{\"video\": \"$id1\", \n \"crf\": $c, \n \"min_length\": 0, \n \"max_length\": $md, \n \"target_seg_length\": \"fix\", \n \"encoder\": \"$encoder\"}"> jobs_2/"$file_id".txt 
 
 			encoder="x265"
-			file_id="$id2"_"$c"-"$md"-"$encoder"-"var"
-			printf "{\"video\": \"$id1\", \n \"crf\": $c, \n \"min_length\": 0, \n \"max_length\": $md, \n \"target_seg_length\": \"var\", \n \"encoder\": \"$encoder\"}"> jobs_2/"$file_id".txt 			
+			file_id="$id2"_"$c"-"$md"-"$encoder"-"fix"
+			printf "{\"video\": \"$id1\", \n \"crf\": $c, \n \"min_length\": 0, \n \"max_length\": $md, \n \"target_seg_length\": \"fix\", \n \"encoder\": \"$encoder\"}"> jobs_2/"$file_id".txt 			
 		
 	done
 done
--- video_encode.sh
+++ video_encode.sh
@@ -73,14 +73,14 @@ echo "### Encoding video (prev. took ${SINCE}s) ###"
 if [[ $target_seg_length == "var" ]]; then
 
 	#encode the video in case of variable
-	ffmpeg -nostats -threads 1 -i $vid_id -crf $crf_val -vcodec lib"$codec" -"$codec"-params keyint="$key_int_max":min-keyint="$key_int_min" -f stream_segment -segment_list $TMP/out.m3u8  $TMP/out_%03d.ts -pass 2
+	ffmpeg -nostats -threads 1 -i $vid_id -crf $crf_val -vcodec lib"$codec" -"$codec"-params "keyint=$key_int_max:min-keyint=$key_int_min" -f stream_segment -segment_list $TMP/out.m3u8  $TMP/out_%03d.ts -pass 2
 	
 
 else
 	#min_dur=$target_seg_length
 	#max_dur=$target_seg_length
 	#encode the video in case of fixed length
-	ffmpeg -nostats -threads 1 -i $vid_id -crf $crf_val -vcodec lib"$codec" -"$codec"-params keyint="$key_int_max":min-keyint="$key_int_min" -f stream_segment -segment_time $max_dur -force_key_frames "expr:gte(t,n_forced*$max_dur)" -segment_list $TMP/out.m3u8  $TMP/out_%03d.ts -pass 2
+	ffmpeg -nostats -threads 1 -i $vid_id -crf $crf_val -vcodec lib"$codec" -"$codec"-params "keyint=$key_int_max:min-keyint=$key_int_min" -f stream_segment -segment_time $max_dur -force_key_frames "expr:gte(t,n_forced*$max_dur)" -segment_list $TMP/out.m3u8  $TMP/out_%03d.ts -pass 2
 	
 
 fi
